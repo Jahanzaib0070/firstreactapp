9807650c19aa6915f149005508bf27c073148e6d
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-23 00:43:50 +0500

    Create App.jsx

---
+++ src/useEffect/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const App = ()=>{
+    const [num, setNum] = useState(0);
+    useEffect(()=>{
+        document.title= `You clicked me ${num} times`
+    })
+
+    return <>
+        <button onClick={()=>{setNum(num+1)}}>
+            Click me {num}
+        </button>
+    </>
+}
+
+export default App;
