26ee66e255b68621518365dcef0a722192bf1550
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-24 13:24:38 +0500

    Update App.jsx

--- src/PokemonAPI/App.jsx
+++ src/PokemonAPI/App.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const App = ()=>{
-    const [num, setNum] = useState();
+const App = () => {
+  const [num, setNum] = useState(1); // Initialize with a default value
+  const [name, setName] = useState("");
+  const [moves, setMoves] = useState(0);
 
-    return <>
-        <h1>You chose {num}</h1>
-        <select value={num} onChange={ (event)=>{
-            setNum(event.target.value)
-        } }>
-            <option value={"1"}>1</option>
-            <option value={"2"}>2</option>
-            <option value={"3"}>3</option>
-            <option value={"4"}>4</option>
-            <option value={"5"}>5</option>
-        </select>
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+        setName(res.data.name);
+        setMoves(res.data.moves.length);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    }
+    getData();
+  });
+
+  return (
+    <>
+      <h1>You chose {num}</h1>
+      <h1>My name is {name}</h1>
+      <h1>I have {moves} moves</h1>
+      <select value={num} onChange={(event) => setNum(event.target.value)}>
+        <option value={"1"}>1</option>
+        <option value={"2"}>2</option>
+        <option value={"3"}>3</option>
+        <option value={"4"}>4</option>
+        <option value={"25"}>25</option>
+      </select>
     </>
-}
+  );
+};
 
 export default App;
